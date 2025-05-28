06efed89723d98b09a2f4d8eef1d6068fff63c23
Clint Rowden <clintonia@gmail.com>
2025-05-27 19:58:19 -0500

    JavaScript Set up function to return PRS. Testing

--- js/ss.js
+++ js/ss.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+let getComputerChoice = ()=>{
+    const randoNumbo = Math.floor(Math.random()*3);
+    switch (randoNumbo) {
+    case 1:
+        console.log("Rock");
+        break;
+    case 2:
+        console.log("Scissors");
+        break;
+    default:
+        console.log("Paper");
+    }
+}
