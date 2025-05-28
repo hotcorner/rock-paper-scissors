e0402f589ecb29d84a6c4310a32c7ca9cb69bf00
Clint Rowden <clintonia@gmail.com>
2025-05-27 21:18:57 -0500

    JS
    Complete game logic for determining winner

--- js/ss.js
+++ js/ss.js
@@ -15,5 +15,63 @@ let getComputerChoice = ()=>{
 
 let getHumanChoice = ()=>{
     const humanChoice = prompt("Paper, Rock or Scissors?");
-    return humanChoice;
+    return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
 }
+
+let playRound = (humanChoice, computerChoice)=>{
+    let result;
+    if(humanChoice=="Paper"){
+        switch(computerChoice){
+            case "Paper":
+                result  = "Tie! Try again.";
+                break;
+            case "Rock":
+                result  = "Paper covers Rock. You win!";
+                humanScore++;
+                break;
+            case "Scissors":
+                result  = "Scissors cuts Paper. Try again.";
+                computerScore++;
+                break;
+        }
+    }
+    else if(humanChoice=="Rock"){
+        switch(computerChoice){
+            case "Paper":
+                result  = "Paper covers Rock. Try again.";
+                computerScore++;
+                break;
+            case "Rock":
+                result  = "Tie! Try again.";
+                break;
+            case "Scissors":
+                result  = "Rock beats Scissors. You win!";
+                humanScore++;
+                break;
+        }
+    }
+    else if(humanChoice=="Scissors"){
+        switch(computerChoice){
+            case "Paper":
+                result  = "Scissors cut paper. You win!";
+                humanScore++;
+                break;
+            case "Rock":
+                result  = "Rock beats Scissors. Try again.";
+                computerScore++;
+                break;
+            case "Scissors":
+                result  = "Tie! Try again.";
+                break;
+        }
+    }
+    console.log(`Human chose ${humanChoice}`);
+    console.log(`Computer chose ${computerChoice}`);
+    console.log(result);
+    console.log(`humans:${humanScore} --- computers:${computerScore}`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
